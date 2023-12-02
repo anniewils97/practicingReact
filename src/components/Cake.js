d25362067516ade05547a81885caf9bf59668a22
Annie Wilson <annemarie.wilson@yahoo.com>
2023-12-02 14:33:14 +0000

    created sell button and did some learning on the impact of curly brackets on functions and in arguments

--- src/components/Cake.js
+++ src/components/Cake.js
@@ -1,9 +1,19 @@
+
 /* in order to display a cake, we need to create a Cake function which takes in a cake object
  as a prop i.e. const Cake (the function) = ({cake}) (the object as a prop, defined specifically
     within this function called Cake).*/
 
-const Cake = ({cake}) => {
+const Cake = ({ cake, onButtonClick }) => {
     // console.log(Cake);
+
+    const handleButton = () => {
+        onButtonClick(cake.cakeName, cake.price);
+        /* initially ran into an error because we had onButtonClick({cake.CakeName, cake.price})
+        in order for the {} to work, they need a key-value pair passed as an argument, but cake.cakeName, cake.price
+        are just values of a property, not a key-value pair.
+        */
+    };
+
     return ( 
         <div className="cake">
         <h3>{cake.cakeName}</h3>
@@ -15,7 +25,7 @@ const Cake = ({cake}) => {
             </ul>
         <p>Price: £{cake.price}</p>
         <p>Rating: {cake.rating}</p>
-        <button>Sell Cake</button>
+        <button onClick={onButtonClick}>Sell Cake</button>
         </div>
      );
 }
